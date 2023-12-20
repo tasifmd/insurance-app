6207d09349d6152eb777480bab9170e462ef272e
tasifmd <tasifmd96@gmail.com>
2023-12-20 13:01:27 +0530

    gateway changes done

--- enroll-service/src/main/java/com/tasif/enrollservice/feign/UserFeignClient.java
+++ enroll-service/src/main/java/com/tasif/enrollservice/feign/UserFeignClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "USER-SERVICE",url = "http://localhost:9090/user")
+@FeignClient(name = "user-service", url = "http://localhost:9090/user")
 public interface UserFeignClient {
 
     @GetMapping("/{username}")
